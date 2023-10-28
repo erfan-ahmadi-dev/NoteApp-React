b71d901a604eb5fed9947812f2759b093b5b2046
erfan ahmadi <dornadev@gmail.com>
2023-10-28 19:24:50 +0330

    components created & app.js refactored to App.jsx

---
+++ src/components/Buttons/RoundedButton.jsx
@@ -0,0 +1,11 @@
+function roundedButton(imgSrc) {
+  return (
+    <img
+      src={imgSrc}
+      alt="button"
+      className="p-2 w-10 h-10 rounded-xl bg-buttonsColor"
+    />
+  );
+}
+
+export default roundedButton;
